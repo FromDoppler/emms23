0c86ffe0538224ecc6c511b6e8ebeb1b2d87f2d1
Matías Roy Rodriguez <mroy@makingsense.com>
2023-02-23 15:19:31 -0300

    feat: add submit logic for homeEccomerce

---
+++ src/1.0.0/js/homeEcommerce.js
@@ -0,0 +1,93 @@
+'use strict';
+
+import {
+    customError,
+    getEncodeURLEmail,
+    searchUrlParam,
+    validateForm,
+} from './common/index.js';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const ecommerceForm = document.getElementById('ecommerceForm');
+    const userEmail = getEncodeURLEmail();
+
+    if (userEmail) {
+        const endPoint = 'exampleEndPointForGetUserEvents';
+        const userData = {
+            email: userEmail
+        }
+
+        const getUserEvents = async () => {
+            //TODO: Poner el endPoint correspondiente cuando este listo
+            const resp = await fetch(endPoint, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData),
+            });
+            const userEvents = await resp.json()
+            return userEvents;
+        }
+
+        const loadUserInterface = (userEvents) => {
+            //TODO: Interactuar con la maqueta cuando este lista
+            console.log('Cargando interfaz');
+        };
+
+        getUserEvents().then(userEvents => {
+            loadUserInterface(userEvents);
+        });
+    }
+
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+        const endPoint = './services/register.php';
+        const formData = new FormData(ecommerceForm);
+        const userData = {
+            'name': formData.get('name'),
+            'email': formData.get('email'),
+            'industry': formData.get('industry'),
+            'acceptPolicies': (formData.get('privacy') === 'true') ? true : null,
+            'acceptPromotions': (formData.get('promotions') === 'true') ? true : null,
+            'utm_source': (searchUrlParam('utm_source') === '') ? 'direct' : searchUrlParam('utm_source'),
+            'utm_campaign': searchUrlParam('utm_campaign'),
+            'utm_content': searchUrlParam('utm_content'),
+            'utm_term': searchUrlParam('utm_term'),
+            'utm_medium': searchUrlParam('utm_medium'),
+            'origin': searchUrlParam('origin'),
+        }
+        const isValidForm = validateForm(ecommerceForm);
+        if (isValidForm)
+            //TODO: Agregar logica en el boton del form cuando este lista la maqueta
+            try {
+                await fetch(endPoint, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(userData),
+                })
+                    .then(resp => resp.json())
+                    .then(resp => {
+                        if (!resp.ok) throw new Error('Server error on eccomerce fetch', resp?.status);
+                        //TODO: Agregar redireccion y cargado de interfaz cuando este lista la maqueta y las url
+                        localStorage.setItem('registered', true);
+                    })
+                    .catch((error) => {
+                        customError('Eccomerce post error', error);
+                    });
+            } catch (error) {
+                customError('Eccomerce fetch error', error);
+            }
+
+
+    }
+
+    ecommerceForm.querySelector('button').addEventListener('click', submitForm);
+
+});
