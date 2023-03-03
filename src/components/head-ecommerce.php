9064c4871936e1ba71f64036293d3a6206c300ff
ornella-688 <ornella.cicalello@gmail.com>
2023-03-03 17:29:16 -0300

    feat(emms): seo content added

---
+++ src/components/head-ecommerce.php
@@ -0,0 +1,13 @@
+<title>EMMS E-commerce: Conferencias gratis y online</title>
+
+<meta name="title" content="EMMS E-commerce: Conferencias gratis y online">
+<meta name="description" content="Inscríbete gratis al mayor evento online sobre E-commerce. Aprende e inspírate con los mayores referentes internacionales, accede a beneficios, contenido exclusivo, ¡y más!">
+
+<meta property="og:url" content="https://goemms.com/">
+<meta property="og:title" content="EMMS E-commerce: Conferencias gratis y onlin">
+<meta property="og:description" content="Inscríbete gratis al mayor evento online sobre E-commerce. Aprende e inspírate con los mayores referentes internacionales, accede a beneficios, contenido exclusivo, ¡y más!">
+<meta property="og:image" content="https://goemms.com/share-ecommerce-2023.png">
+<meta name="twitter:image" content="https://goemms.com/share-ecommerce-2023.png">
+
+<meta property="og:site_name" content="EMMS E-commerce: Conferencias gratis y online">
+<meta name="twitter:image:alt" content="EMMS E-commerce: Conferencias gratis y online">
