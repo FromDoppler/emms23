de68059f03426ebb775bcf519665b6abe90154e6
mcappato <mcappato@makingsense.com>
2023-03-09 15:40:47 -0300

    chore: serve add get and remove sponsor endpoints

---
+++ adm23/server/modules/sponsors/removeSponsor.php
@@ -0,0 +1,12 @@
+<?php
+include_once '../../config/config.php';
+header('Content-Type: application/json; charset=utf-8');
+
+if (isset($_POST['sponsorId'])) {
+    $sql_query = "DELETE FROM sponsors WHERE id=" . $_POST['sponsorId'];
+    $sql_query = "UPDATE sponsors SET `status`='0' WHERE sponsor_id=" . $_POST['sponsorId'];
+    mysqli_query($con, $sql_query);
+    echo json_encode($sql_query);
+} else {
+    echo "error";
+}
