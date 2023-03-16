e320e5fb82b22e9b912244a5423d91f35275f2cb
ornella-688 <ornella.cicalello@gmail.com>
2023-03-16 16:05:05 -0300

    feat(sponsors): layout

---
+++ src/components/doppler-academy-banner.php
@@ -0,0 +1,24 @@
+<section class="emms__doppleracademybanner">
+    <div class="emms__container--lg">
+        <div class="emms__doppleracademybanner__image emms__fade-in">
+            <img src="src/img/woman-picture-b.png" alt="Doppler">
+        </div>
+        <div class="emms__doppleracademybanner__description emms__fade-in">
+            <h2>Aprende con Doppler Academy</h2>
+            <p><strong>Doppler</strong> es la <strong>herramienta líder en Marketing Automation</strong> en español.</p>
+            <p>Además de ayudar al crecimiento de tu negocio, formamos a profesionales de Marketing a lo largo de todo el mundo de forma <strong>100% gratuita</strong>.</p>
+            <p>Desde hace 15 años organizamos el EMMS y también ofrecemos cursos, entrenamientos y recursos en nuestra academia de capacitación online.</p>
+            <div class="emms__doppleracademybanner__description__bottom">
+                <h3><strong>¿Quieres capacitarte gratis?</strong> ¡Esto te va a interesar! </h3>
+                <p>Descubre Doppler Digital Bootcamp y certifícate ahora en Automation Marketing.</p>
+                <ul>
+                    <li>Gratis</li>
+                    <li>Online y a tu ritmo</li>
+                    <li>20 lecciones de 15 minutos</li>
+                    <li>Clases teóricas y prácticas</li>
+                </ul>
+                <a href="" class="emms__cta emms__fade-in">ACCEDE AHORA</a>
+            </div>
+        </div>
+    </div>
+</section>
