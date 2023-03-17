65b33537f2cd7c8ebc8efeaf5965ab5b222e3fd1
ornella-688 <ornella.cicalello@gmail.com>
2023-03-17 17:48:00 -0300

    fix(home): content SEO changed

--- src/components/head-home.php
+++ src/components/head-home.php
@@ -1,11 +1,11 @@
 <title>EMMS 2023: Conferencias online y gratuitas sobre Marketing Digital</title>
 
 <meta name="title" content="EMMS 2023: Conferencias online y gratuitas sobre Marketing Digital">
-<meta name="description" content="Súmate gratis y en un mismo lugar a 3 ediciones del evento de Marketing más esperado del año: E-commerce, Fintech y tendencias digitales.">
+<meta name="description" content="Súmate gratis y en un mismo lugar a 2 ediciones del evento de Marketing más esperado del año: E-commerce y Digital Trends.">
 
 <meta property="og:url" content="https://goemms.com/">
 <meta property="og:title" content="EMMS 2023: Conferencias online y gratuitas sobre Marketing Digital">
-<meta property="og:description" content="Súmate gratis y en un mismo lugar a 3 ediciones del evento de Marketing más esperado del año: E-commerce, Fintech y tendencias digitales.">
+<meta property="og:description" content="Súmate gratis y en un mismo lugar a 2 ediciones del evento de Marketing más esperado del año: E-commerce y Digital Trends.">
 <meta property="og:image" content="https://goemms.com/share-2023.png">
 <meta name="twitter:image" content="https://goemms.com/share-2023.png">
 
