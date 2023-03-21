7eb0c015575089833b4bda49a6f05fdcc48223d9
Matías Roy Rodriguez <mroy@makingsense.com>
2023-03-21 17:02:11 -0300

    feat: implement sponsors interna

--- sponsors-interna.php
+++ sponsors-interna.php
@@ -1,5 +1,23 @@
 <?php
-require_once('././config.php');
+require_once('./config.php');
+require_once('./utils/DB.php');
+
+if (isset($_GET['slug'])) {
+    $db = new DB(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $sponsor = $db->getSponsorsBySlug($_GET['slug']);
+    if (count($sponsor) != 0) {
+        $sponsor = $sponsor[0];
+        echo '<code>';
+        var_dump($sponsor);
+        echo '</code>';
+    } else {
+        header('Location: ' . 'sponsors');
+    }
+} else {
+    header('Location: ' . 'sponsors');
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -19,7 +37,7 @@ require_once('././config.php');
                 <a href="./index.php"><img src="src/img/logos/logo-emms.png" alt="Emms 2023"></a>
             </div>
             <div class="emms__header__logo">
-                <a href="./index.php"><img src="src/img/logos/logo-siteground.png" alt="Siteground"></a>
+                <a href="./index.php"><img src="./adm23/server/modules/sponsors/uploads/<?= $sponsor['logo_company'] ?>" alt="<?= $sponsor['alt_logo_company'] ?>"></a>
             </div>
         </div>
     </header>
@@ -31,11 +49,15 @@ require_once('././config.php');
             <div class="emms__background-a"></div>
             <div class="emms__container--lg emms__fade-top">
                 <div class="emms__internal-sponsors__hero__content">
-                    <h1>Aquí va el Título de la cápsula.</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    <h1><?= $sponsor['title'] ?></h1>
+                    <p><?= $sponsor['description'] ?></p>
                 </div>
                 <div class="emms__internal-sponsors__hero__video">
-                    <video src="src/img/video-ejemplo.mp4" controls></video>
+                    <?php if (!empty($sponsor['image_youtube'])) :  ?>
+                        <video src="<?= $sponsor['youtube'] ?>" controls></video>
+                    <?php else : ?>
+                        <img src="./adm23/server/modules/sponsors/uploads/<?= $sponsor['image_youtube'] ?>" alt="<?= $sponsor['alt_image_youtube'] ?>" />
+                    <?php endif; ?>
                 </div>
             </div>
         </section>
@@ -47,9 +69,9 @@ require_once('././config.php');
                     <img src="src/img/download.png" alt="download">
                 </div>
                 <div class="emms__internal-sponsors__resource__text">
-                    <h2>Aquí va el título del Lead Magnet del Sponsor</h2>
-                    <p>Aquí va una descripción del Recurso. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                    <a href="" class="emms__cta">ACCEDER</a>
+                    <h2><?= $sponsor['title_magnet'] ?></h2>
+                    <p><?= $sponsor['description_magnet'] ?></p>
+                    <a href="<?= $sponsor['link_magnet'] ?>" class="emms__cta">ACCEDER</a>
                 </div>
             </div>
         </section>
@@ -58,9 +80,9 @@ require_once('././config.php');
         <section class="emms__internal-sponsors__description">
             <div class="emms__background-a"></div>
             <div class="emms__container--md emms__fade-in">
-                <h2>Conoce más sobre “Nombre del Sponsor”</h2>
-                <p>Aquí va una descripción del Sponsor. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-                <a href="" class="emms__cta">CONOCE MÁS</a>
+                <h2>Conoce más sobre <?= $sponsor['title_promo_company'] ?></h2>
+                <p><?= $sponsor['description_promo_company'] ?></p>
+                <a href="<?= $sponsor['link_promo_company'] ?>" class="emms__cta">CONOCE MÁS</a>
             </div>
         </section>
 
