d3f4909107586bdd064c446110ae6cbfae99837c
Matías Roy Rodriguez <mroy@makingsense.com>
2023-04-17 22:07:57 -0300

    feat: add multiple events in local storage

--- src/1.0.0/js/common/submitForm.js
+++ src/1.0.0/js/common/submitForm.js
@@ -7,7 +7,7 @@ import {
     toHex,
 } from './index.js';
 
-const submitFormFetch = async (form, fetchType = null) => {
+const submitFormFetch = async (form, fetchType) => {
 
     const endPoint = './services/register.php';
     const formData = new FormData(form);
@@ -25,10 +25,14 @@ const submitFormFetch = async (form, fetchType = null) => {
         'utm_medium': searchUrlParam('utm_medium'),
         'origin': searchUrlParam('origin'),
         'type': fetchType,
+        'events': ''
     };
     const isValidForm = validateForm(form);
     if (isValidForm) {
         form.querySelector('button').classList.add('button--loading');
+
+        userData.events = setEventInLocalStorage(fetchType, encodeEmail);
+
         try {
             const fetchResp = await fetch(endPoint, {
                 method: 'POST',
@@ -38,16 +42,30 @@ const submitFormFetch = async (form, fetchType = null) => {
                 body: JSON.stringify(userData),
             });
             return {
-                fetchResp,
-                encodeEmail
+                fetchResp
             };
         } catch (error) {
-            customError('Eccomerce fetch error', error);
+            customError('Fetch error', error);
         }
         form.querySelector('button').classList.remove('button--loading');
     }
 
 };
+
+const setEventInLocalStorage = (fetchType, encodeEmail) => {
+    let events;
+    if (localStorage.getItem('events')) {
+        events = JSON.parse(localStorage.getItem('events'));
+        events.push(fetchType);
+        localStorage.setItem('events', JSON.stringify(events));
+    } else {
+        events = [fetchType];
+        localStorage.setItem('dplrid', encodeEmail);
+        localStorage.setItem('events', JSON.stringify(events));
+    }
+    localStorage.setItem('lastEventsUpdateTime', new Date());
+    return JSON.stringify(events);
+}
 
 export {
     submitFormFetch
