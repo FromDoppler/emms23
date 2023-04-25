5dc96918abd04fdd19956ff72aaa7794c9bc2cb8
Matías Roy Rodriguez <mroy@makingsense.com>
2023-04-25 11:04:08 -0300

    feat: change in counter to print in multiple places

--- src/components/date-counter.php
+++ src/components/date-counter.php
@@ -1,19 +1,19 @@
 <ul class="emms__counter">
     <h5>FALTAN</h5>
     <li class="emms__counter__number">
-        <div><span id="d"></span></div>
+        <div><span class="d"></span></div>
         <span class="emms__counter__number--data">días</span>
     </li>
     <li class="emms__counter__number">
-        <div><span id="h"></span></div>
+        <div><span class="h"></span></div>
         <span class="emms__counter__number--data">horas</span>
     </li>
     <li class="emms__counter__number">
-        <div><span id="m"></span></div>
+        <div><span class="m"></span></div>
         <span class="emms__counter__number--data">minutos</span>
     </li>
     <li class="emms__counter__number">
-        <div><span id="s"></span></div>
+        <div><span class="s"></span></div>
         <span class="emms__counter__number--data">segundos</span>
     </li>
 </ul>
