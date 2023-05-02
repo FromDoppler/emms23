dc079546a3c6788d7402ec9d64654bfce52963f1
Matías Roy Rodriguez <mroy@makingsense.com>
2023-05-02 17:27:41 -0300

    feat: add new submit type without form when user is already registered

--- src/1.0.0/js/homeEcommerce.js
+++ src/1.0.0/js/homeEcommerce.js
@@ -4,31 +4,52 @@ import {
     customError,
     getUrlWithParams,
     submitFormFetch,
+    submitWithoutForm,
 } from './common/index.js';
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     const ecommerceForm = document.getElementById('ecommerceForm');
+    const ecommerceBtn = document.getElementById('ecommerceBtn');
 
-    const submitForm = async (e) => {
+    if (ecommerceForm) {
+        const submitForm = async (e) => {
 
-        e.preventDefault();
+            e.preventDefault();
 
-        await submitFormFetch(ecommerceForm, 'ecommerce').then(({ fetchResp: resp}) => {
-            if (!resp.ok) throw new Error('Server error on eccomerce fetch', resp?.status);
+            await submitFormFetch(ecommerceForm, 'ecommerce').then(({ fetchResp: resp }) => {
+                if (!resp.ok) throw new Error('Server error on eccomerce fetch', resp?.status);
 
-            window.location.href = getUrlWithParams('/ecommerce-registrado.php');
-            if (window.location.pathname === '/sponsors.php') {
-                window.location.href = getUrlWithParams('/sponsors-registrado.php');
-            }
-        })
-            .catch((error) => {
-                customError('Eccomerce post error', error);
+                window.location.href = getUrlWithParams('/ecommerce-registrado.php');
+                if (window.location.pathname === '/sponsors.php') {
+                    window.location.href = getUrlWithParams('/sponsors-registrado.php');
+                }
+            })
+                .catch((error) => {
+                    customError('Eccomerce post error', error);
+                });
+
+
+        }
+
+        ecommerceForm.querySelector('button').addEventListener('click', submitForm);
+    }
+    if (ecommerceBtn) {
+        const submitEvent = async (e) => {
+            ecommerceBtn.classList.add('button--loading');
+            e.preventDefault();
+            await submitWithoutForm('ecommerce').then(({ fetchResp: resp }) => {
+                if (!resp.ok) throw new Error('Server error on digital fetch', resp?.status);
+                ecommerceBtn.classList.remove('button--loading');
+
+                window.location.href = getUrlWithParams('/ecommerce-registrado.php');
+                if (window.location.pathname === '/sponsors.php') {
+                    window.location.href = getUrlWithParams('/sponsors-registrado.php');
+                }
             });
-
-
+        }
+        ecommerceBtn.addEventListener('click', submitEvent);
     }
 
-    ecommerceForm.querySelector('button').addEventListener('click', submitForm);
 });
