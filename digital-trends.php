b4714e635732cc315a2e5d82ea9e049a2d2c7475
Matías Roy Rodriguez <mroy@makingsense.com>
2023-05-02 17:30:47 -0300

    feat: add redirect for user events

--- digital-trends.php
+++ digital-trends.php
@@ -2,7 +2,19 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/services/functions.php');
-
+?>
+<script type="module">
+    import {
+        getUrlWithParams
+    } from './src/<?= VERSION ?>/js/common/index.js';
+    import {
+        userRegisteredInEvent,
+    } from './src/<?= VERSION ?>/js/user.js';
+    if (userRegisteredInEvent('digital-trends')) {
+        window.location.href = getUrlWithParams('/digital-trends-registrado');
+    }
+</script>
+<?
 $response = processPhaseToShow('digital-trends');
 require_once($_SERVER['DOCUMENT_ROOT'] . "/stages/digital-trends/$response[phaseToShow]/digital-trends.php");
 ?>
--- ecommerce.php
+++ ecommerce.php
@@ -2,7 +2,19 @@
 
 require_once($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/services/functions.php');
-
+?>
+<script type="module">
+    import {
+        getUrlWithParams
+    } from './src/<?= VERSION ?>/js/common/index.js';
+    import {
+        userRegisteredInEvent
+    } from './src/<?= VERSION ?>/js/user.js';
+    if (userRegisteredInEvent('ecommerce')) {
+        window.location.href = getUrlWithParams('/ecommerce-registrado');
+    }
+</script>
+<?
 $response = processPhaseToShow('ecommerce');
 require_once($_SERVER['DOCUMENT_ROOT'] . "/stages/ecommerce/$response[phaseToShow]/ecommerce.php");
 ?>
