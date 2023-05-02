fb69122598429d55f89f050fdb10cbfbea387ee6
Matías Roy Rodriguez <mroy@makingsense.com>
2023-05-02 17:28:59 -0300

    feat: user events check in header

--- src/components/head.php
+++ src/components/head.php
@@ -45,3 +45,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M768WZR');</script>
 <!-- End Google Tag Manager -->
+
+<script type="module">
+        import {
+            checkEncodeUrl
+        } from '/src/<?= VERSION ?>/js/user.js';
+        checkEncodeUrl();
+</script>
