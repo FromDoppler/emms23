54d932e321f85820005c1ef2a854cd5a22ce49a7
mcappato <mcappato@makingsense.com>
2023-05-09 09:33:42 -0300

    chore: transmission component

--- adm23/src/modules/settings/phases/services/setPhase.js
+++ adm23/src/modules/settings/phases/services/setPhase.js
@@ -14,3 +14,19 @@ export const setPhase = async (currentEvent, selectedPhase, transition) => {
         console.log(error);
     }
 };
+
+export const setTransmission = async (currentEvent, transmission) => {
+    try {
+        const setPhaseUrl = '/adm23/server/modules/settings/setTransmission.php';
+        const formData = new FormData();
+        formData.append("event", currentEvent);
+        formData.append("transmission", transmission);
+
+        await fetch(setPhaseUrl, {
+            method: "post",
+            body: formData
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
