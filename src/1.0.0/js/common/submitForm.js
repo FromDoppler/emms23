7975900557f6a54d9fda646655f5758f12ad522f
Matías Roy Rodriguez <mroy@makingsense.com>
2023-09-18 09:04:05 -0300

    feat: send phone values if exist

--- src/1.0.0/js/common/submitForm.js
+++ src/1.0.0/js/common/submitForm.js
@@ -17,11 +17,13 @@ const submitFormFetch = async (form, fetchType) => {
         'userEvents': []
     }
     const encodeEmail = toHex(formData.get('email'));
+    const { dialCode } = iti.s;
     const userData = {
         'name': formData.get('name'),
         'email': formData.get('email'),
         'company': formData.get('company'),
         'position': formData.get('position'),
+        'phone': (formData.get('phone').trim() != '') ? dialCode + formData.get('phone') : null,
         'encodeEmail': encodeEmail,
         'acceptPolicies': (formData.get('privacy') === 'true') ? true : null,
         'acceptPromotions': (formData.get('promotions') === 'true') ? true : null,
