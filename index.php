6684f37fde375674926bba4b33df4ecbdc01802e
ornella-688 <ornella.cicalello@gmail.com>
2023-09-27 15:24:16 -0300

    fix(navbar): removed entradas menu

--- index.php
+++ index.php
@@ -62,7 +62,6 @@ require_once('././src/components/cacheSettings.php');
                     <li><a href="./digital-trends">digital trends</a></li>
                     <li><a href="/sponsors">biblioteca de recursos</a></li>
                     <li><a href="/ediciones-anteriores">ediciones anteriores</a></li>
-                    <li><a href="./digital-trends#entradas" target="_blank">entradas</a></li>
                 </ul>
             </nav>
         </div>
