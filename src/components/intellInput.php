84a56e5504b94db2807b9b655ca8f1c3d273ef44
Matías Roy Rodriguez <mroy@makingsense.com>
2023-09-28 12:48:26 -0300

    fix: add dial code to intell input

--- src/components/intellInput.php
+++ src/components/intellInput.php
@@ -1,11 +1,15 @@
-   <script type="module">
-       import {
-           setPhoneFlagByUserIp
-       } from "./src/<?= VERSION ?>/js/phoneFlag.js";
-
+   <script>
        const input = document.querySelector("#phone");
        const iti = window.intlTelInput(input, {
+           hiddenInput: "full_phone",
+           separateDialCode: true,
            utilsScript: "src/<?= VERSION ?>/js/vendors/utils.js",
+           initialCountry: "auto",
+           geoIpLookup: callback => {
+               fetch("https://ipapi.co/json")
+                   .then(res => res.json())
+                   .then(data => callback(data.country_code))
+                   .catch(() => callback("us"));
+           }
        });
-       setPhoneFlagByUserIp(input);
    </script>
