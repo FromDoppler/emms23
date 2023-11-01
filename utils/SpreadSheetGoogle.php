656da9d32f9f752739dc145e753f1d0a5c6cbc5f
Matías Roy Rodriguez <mroy@makingsense.com>
2023-11-01 15:09:12 -0300

    feat: add new fields in spread

--- utils/SpreadSheetGoogle.php
+++ utils/SpreadSheetGoogle.php
@@ -1,30 +1,35 @@
 <?php
 require_once 'spread/write.php';
 
-class SpreadSheetGoogle {
+class SpreadSheetGoogle
+{
+    const RANGE = 'A1:R1';
 
-    public static function write($idSpreadSheet, $user, $db) {
+    public static function write($idSpreadSheet, $user, $db)
+    {
         date_default_timezone_set('America/Argentina/Buenos_Aires');
-        $range = 'A1:R1';
-        $values = array( array(
-            date('h:i:s A'),
-            date('d-m-Y'),
-            $user['promotions'],
-            $user['privacy'],
-            $user['firstname']. " ",
-            $user['email'],
-            // $user['company']. " ",
-            // $user['jobPosition']. " ",
-            $user['source_utm'] . " ",
-            $user['medium_utm'] . " ",
-            $user['campaign_utm'] . " ",
-            $user['content_utm'] . " ",
-            $user['term_utm'] . " ",
-            $user['origin'] . " ",
-            $user['country_ip'] . " ",
-            $user['ecommerce'] . " ",
-            $user['digital_trends'] . " ",
-        ));
-        write_to_sheet($idSpreadSheet, $range, $values, $db);
+        $values = array(
+            array(
+                date('h:i:s A'),
+                date('d-m-Y'),
+                $user['promotions'],
+                $user['privacy'],
+                $user['firstname'] . " ",
+                $user['email'],
+                $user['source_utm'] . " ",
+                $user['medium_utm'] . " ",
+                $user['campaign_utm'] . " ",
+                $user['content_utm'] . " ",
+                $user['term_utm'] . " ",
+                $user['origin'] . " ",
+                $user['country_ip'] . " ",
+                $user['ecommerce'] . " ",
+                $user['digital_trends'] . " ",
+                $user['company'] . " ",
+                $user['jobPosition'] . " ",
+                $user['phone'] . " ",
+            )
+        );
+        write_to_sheet($idSpreadSheet, self::RANGE, $values, $db);
     }
 }
